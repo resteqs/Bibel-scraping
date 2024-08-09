d02a00082ebb5aefa0aa037a71250caf153f02c1
resteqs <antoni.mackowiak@stud-mail.uni-wuerzburg.de>
2024-08-09 08:58:23 +0200

    Update for whole langugae

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -1,23 +1,44 @@
-from pathlib import Path
-
 import scrapy
+import re
+#POLNCV has 693 lines
+class BibleSpider(scrapy.Spider):
+    name = "holyjesus"
 
+    def start_requests(self):
+        yield scrapy.Request(url=f"https://live.bible.is/bible/POLNCV/MAT/1", callback=self.parse_chapter)
 
-class QuotesSpider(scrapy.Spider):
-    name = "quotes"
-    start_urls = [
-        "https://quotes.toscrape.com/page/1/",
-    ]
+    def parse_chapter(self, response):
+        js = response.css("script::text").get()
+        pattern = re.compile(r'"([A-Z0-9]+)"(?=:"(?:OT|NT)")')  # Only books from new testament
+        booknames = pattern.findall(js)
+        booknames = list(dict.fromkeys(booknames))
+        self.log(f'Extracted chapter names: {booknames}')
+        
+        for book in booknames:
+            url = f'https://live.bible.is/bible/POLNCV/{book}/1?audio_type=audio_drama'
+            yield scrapy.Request(url, callback=self.parse)
 
     def parse(self, response):
-        for quote in response.css("div.quote"):
-            yield {
-                "text": quote.css("span.text::text").get(),
-                "author": quote.css("small.author::text").get(),
-                "tags": quote.css("div.tags a.tag::text").getall(),
-            }
+        verses = response.xpath('//div[@class="chapter justify"]//span[contains(@class, "align-left") and @data-verseid]')
+        bible = response.xpath('//h1[@class="version-text"]/text()').get()
+        book = response.url.split('/')[5]
+        chapter = response.url.split('/')[6].split('?')[0]
+        language = response.url.split('/')[4]
+        counter = 0
+        for verse in verses:
+            counter = counter + 1
+            verse_number = verse.xpath('@data-verseid').get()
+            verse_text = verse.xpath('.//span/text()').get(default="not-found")
 
-        next_page = response.css("li.next a::attr(href)").get()
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
+            yield {
+                'language': language,
+                'bible': bible,
+                'book': book,
+                'chapter': chapter,
+                'verse_number': verse_number,
+                'verse_text': verse_text,
+            }
+        if counter != 0:
+            new_chapter = int(chapter) + 1
+            next_page_url = f'https://live.bible.is/bible/POLNCV/{book}/{new_chapter}?audio_type=audio_drama'
+            yield scrapy.Request(next_page_url, callback=self.parse)
